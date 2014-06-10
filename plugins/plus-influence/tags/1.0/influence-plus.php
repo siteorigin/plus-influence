885e04ffe634082dafd071bfa9d83e3c9b49c7e7
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2014-06-10 10:29:44 +0000

    Tagged Influence plus plugin.

---
+++ plugins/plus-influence/tags/1.0/influence-plus.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Plugin Name: Influence Plus
+Description: Extensions and enhancements to the Influence WordPress theme.
+Version: trunk
+Author: Greg Priday
+Author URI: http://siteorigin.com
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl.html
+*/
+
+function influence_plus_init(){
+
+	if( get_option('template') == 'influence' && function_exists( 'siteorigin_setting' ) ) {
+		include plugin_dir_path(__FILE__) . '/customizer/customizer.php';
+		include plugin_dir_path(__FILE__) . '/main.php';
+
+		// Add the updates plugin
+		include plugin_dir_path(__FILE__) . '/wp-updates.php';
+		new WPUpdatesPluginUpdater_467( 'http://wp-updates.com/api/2/plugin', plugin_basename(__FILE__) );
+	}
+}
+add_action('init', 'influence_plus_init', 1);
---
+++ plugins/plus-influence/tags/1.0/main.php
@@ -0,0 +1,63 @@
+<?php
+
+// Do this to remove notifications
+if( !defined('SITEORIGIN_IS_PREMIUM') ) define('SITEORIGIN_IS_PREMIUM', true);
+
+include plugin_dir_path(__FILE__) . '/customizer.php';
+
+/**
+ * Add the missing settings
+ */
+function influence_plus_theme_settings(){
+	siteorigin_settings_add_field( 'logo', 'retina_logo', 'media' );
+	siteorigin_settings_add_field( 'general', 'attribution', 'checkbox' );
+	siteorigin_settings_add_field( 'home', 'slider_shortcode', 'text' );
+}
+add_action('admin_init', 'influence_plus_theme_settings', 15);
+
+/**
+ * Add the retina logo attribute
+ *
+ * @param $attr
+ *
+ * @return mixed
+ */
+function influence_plus_add_retina_logo($attr){
+	$logo = siteorigin_setting('logo_retina_logo');
+
+	if( !empty($logo) ) {
+		$image = wp_get_attachment_image_src($logo, 'full');
+		$attr['data-retina-image'] = $image[0];
+	}
+
+	return $attr;
+}
+add_filter('influence_logo_image_attributes', 'influence_plus_add_retina_logo');
+
+/**
+ * Remove attribution
+ *
+ * @param $credits
+ *
+ * @return string
+ */
+function influence_plus_filter_attribution($credits){
+	if( !siteorigin_setting('general_attribution') ) return '';
+	else return $credits;
+}
+add_filter('influence_credits_siteorigin', 'influence_plus_filter_attribution');
+
+/**
+ * Display the shortcode slider
+ *
+ * @param $code
+ *
+ * @return string
+ */
+function influence_plus_display_slider($code){
+	if( !is_front_page() || siteorigin_setting('home_displays') != 'shortcode' || !siteorigin_setting('home_slider_shortcode') ) return $code;
+
+	$code = '<div id="under-masthead-slider" ' . ( siteorigin_setting('home_slider_margin') ? '' : 'class="remove-bottom-margin"' ) . '>' . do_shortcode( siteorigin_setting('home_slider_shortcode') ) . '</div>';
+	return $code;
+}
+add_filter('influence_after_header', 'influence_plus_display_slider');
